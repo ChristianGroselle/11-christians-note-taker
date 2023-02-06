07cdb7cc00b27a6e9752656bc8b7ad9c66e535a2
Christian Groselle <christian.groselle@gmail.com>
2023-02-06 16:26:50 -0500

    small changes to server.js to make it heroku ready

--- server.js
+++ server.js
@@ -2,10 +2,8 @@ const express = require("express");
 const path = require("path");
 const api = require("./routes/index.js");
 
-const fs = require("fs");
-
 const app = express();
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 // Middleware for parsing JSON and urlencoded form data
 app.use(express.json());
